dded3c640b31a49ee5b6ff7f33a1e9b6c74fda92
Wilson Bell <wilsontbell@gmail.com>
2022-09-08 13:21:49 -0500

    Robber Classes built

--- Interface.cs
+++ Interface.cs
@@ -2,12 +2,9 @@ namespace HeistP2
 {
     public interface IRobber
     {
-        string Name {get; set;}
-        int SkillLevel {get; set;}
-        int PercentageCut {get; set;}
-        void PerformSkill(Bank bank)
-        {
-            
-        }
+        string Name { get; set; }
+        int SkillLevel { get; set; }
+        int PercentageCut { get; set; }
+        void PerformSkill(Bank bank);
     }
 }
---
+++ LockSpecialist.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace HeistP2
+{
+    public class LockSpecialist : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(Bank bank)
+        {
+            bank.VaultScore -= SkillLevel;
+
+            Console.WriteLine($"{Name} is breaking in, and has lowered the Bank's vault score by {SkillLevel}");
+
+            if (bank.VaultScore <= 0)
+            {
+                Console.WriteLine($"{Name} has cracked the safe");
+            }
+        }
+    }
+
+}
